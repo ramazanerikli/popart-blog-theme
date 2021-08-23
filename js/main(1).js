03ca8bf3103cf74f81623b3a759637bde2e0a8f8
Ramazan Erikli <ramazanerikli1@gmail.com>
2021-08-23 16:13:15 +0200

    Edit folder structure

--- js/main(1).js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('#toggle')
-  const menu = document.querySelector('.nav-menu');
-
-  toggle.addEventListener('click', () => {
-    menu.classList.toggle('is-visible');
-  });
-});
